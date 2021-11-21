01a24b6047785860b5794ee96d7733b548b8d8b8
Placido81 <markyo81@gmail.com>
2021-11-21 16:29:23 -0500

    Phone Book

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = (props) => {
+  const people = [
+    { name: "Grid Flex", number: "040-123456", id: 1 },
+    { name: "CyberGirl", number: "944-532-3523", id: 2 },
+    { name: "411Jackson", number: "305-237-4345", id: 3 },
+    { name: "Error Bugs", number: "393-642-3122", id: 4 }
+  ];
+
+  const [search, setNewSearch] = useState("");
+  const handleSearchChange = (e) => {
+    setNewSearch(e.target.value);
+};
+const filtered = !search
+    ? people
+    : people.filter((person) =>
+        person.name.toLowerCase().includes(search.toLowerCase())
+      );
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Phone Book</h2>
+      <input type="text" value={search} onChange={handleSearchChange} />
+
+      <h2>Numbers Of The Team</h2>
+      {filtered.map((person) => {
+        return (
+            <p key={person.id}>
+                {person.name} - {person.number}
+            </p>
+        );
+    })}
+    
+    </>
   );
-}
+};
 
 export default App;
